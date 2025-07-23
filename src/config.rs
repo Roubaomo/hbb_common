520ba12a2acb3104ffb2aa7b080711cb99115849
Roubaomo <105277377+Roubaomo@users.noreply.github.com>
2025-07-24 01:54:47 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["home.rbtv.top:22226"];
+pub const RS_PUB_KEY: &str = "+LBWrddjUrTEE9nG6sg2Q2zEZzimOFepvzuQxLwC1a8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
